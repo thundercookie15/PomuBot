ae75ac95795b5575056c7b5e7c25fa874cbcd9bd
lucas <lucas.maijers@student.rocvf.nl>
2023-08-02 10:31:37 +0200

    Add log channel message if bot is added to a server.

--- src/core/events/guildCreate.ts
+++ src/core/events/guildCreate.ts
@@ -1,6 +1,9 @@
 import {Guild} from 'discord.js'
 import {log} from '../../helpers'
+import {client} from "../lunaBotClient";
 
 export function guildCreate(guild: Guild) {
   log(`${guild.name} (${guild.id}) added the bot. (Owner: ${guild.ownerId})`)
+  const ch = client.channels.cache.get('1101510606261075978')
+  ch?.isTextBased() && ch.send(`**${guild.name}** (${guild.id}) added the bot. (Owner: ${guild.ownerId})`)
 }
--- src/core/events/guildDelete.ts
+++ src/core/events/guildDelete.ts
@@ -1,9 +1,12 @@
 import {Guild} from 'discord.js'
 import {log} from '../../helpers'
 import {deleteGuildData, deleteGuildSettings} from '../db/functions'
+import {client} from "../lunaBotClient";
 
 export function guildDelete(guild: Guild) {
   log(`${guild.name} (${guild.id}) left. Data and settings cleared.)`)
+  const ch = client.channels.cache.get('1101510606261075978')
+  ch?.isTextBased() && ch.send(`**${guild.name}** (${guild.id}) left. Data and settings cleared.`)
 
   deleteGuildData(guild.id)
   deleteGuildSettings(guild.id)
