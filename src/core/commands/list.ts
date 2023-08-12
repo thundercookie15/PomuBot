646b53ccceabfbe4d9bec3a7266cac8e058c63e1
lucas <lucas.maijers@student.rocvf.nl>
2023-08-12 20:22:22 +0200

    Fix the list command and have it filtered by agency.

--- src/core/commands/list.ts
+++ src/core/commands/list.ts
@@ -1,9 +1,9 @@
 import {SlashCommandBuilder} from '@discordjs/builders'
-import {CommandInteraction} from 'discord.js'
+import {ChatInputCommandInteraction, CommandInteraction} from 'discord.js'
 import {Command, createEmbed, reply} from '../../helpers/discord'
-import {getStreamerList} from '../db/streamers/'
+import {getStreamerList, streamerGroups} from '../db/streamers/'
 
-const description = 'Lists supported YT channels'
+const description = 'Lists supported VTuber Agencies and indies and their VTubers.'
 
 export const list: Command = {
   config: {
@@ -13,14 +13,67 @@ export const list: Command = {
     category: 'General',
     description: 'Lists supported YT channels.',
   },
-  slash: new SlashCommandBuilder().setName('list').setDescription(description),
-  callback: (intr: CommandInteraction): void => {
+  slash: new SlashCommandBuilder().setName('list').setDescription(description)
+    .addStringOption((option) => option.setName('group').setDescription('VTuber Agency').
+    addChoices(
+      {
+        "name": 'Hololive',
+        "value": 'Hololive',
+      },
+      {
+        "name": 'Nijisanji',
+        "value": 'Nijisanji',
+      },
+      {
+        "name": 'Nijisanji JP',
+        "value": 'Nijisanji JP',
+      },
+      {
+        "name": 'Nijisanji ID',
+        "value": 'Nijisanji ID',
+      },
+      {
+        "name": 'Nijisanji EN',
+        "value": 'Nijisanji EN',
+      },
+      {
+        "name": 'Nijisanji KR',
+        "value": 'Nijisanji KR',
+      },
+      {
+        "name": 'Indies',
+        "value": 'Independent',
+      },
+      {
+        "name": 'Idol',
+        "value": 'Idol',
+      },
+      {
+        "name": 'Prism',
+        "value": 'Prism Project',
+      },
+      {
+        "name": 'Phase Connect',
+        "value": 'Phase Connect',
+      }
+    ).setRequired(false)),
+  callback: (intr: ChatInputCommandInteraction): void => {
+    const group = intr.options.getString('group')
+    if (group === null) {
+      reply(
+        intr,
+        createEmbed({
+          title: 'Supported Agencies and Indies',
+          description: streamerGroups.join('\n'),
+        }),
+      )
+      return
+    }
     reply(
       intr,
       createEmbed({
-        title: 'Supported channels',
-        description: getStreamerList(),
-      }),
-    )
+        title: `${group} VTubers`,
+        description: getStreamerList(group!),
+      }))
   },
 }
--- src/core/db/streamers/index.ts
+++ src/core/db/streamers/index.ts
@@ -1,5 +1,5 @@
 /** @file Exports main streamer list and streamer-related utility functions */
-import {CommandInteraction, Message} from 'discord.js'
+import {CommandInteraction} from 'discord.js'
 import {ciEquals} from '../../../helpers'
 import {createEmbed, reply} from '../../../helpers/discord'
 import {YouTubeChannelId} from '../../../modules/holodex/frames'
@@ -16,10 +16,25 @@ import {phase_connect} from './phaseconnect'
 import {prism_project} from "./prismproject";
 
 export const streamers = StreamerArray([...nijisanji, ...nijisanji_id, ...nijisanji_kr, ...nijisanji_jp, ...hololive, ...IdolComp, ...indies, ...phase_connect, ...prism_project, ...vshojo] as const)
-
+export const streamerGroups = ['Hololive', 'Nijisanji', "Nijisanji EN", 'Nijisanji ID', 'Nijisanji KR', 'Nijisanji JP', 'Indies', 'Idol', 'VShojo', 'Phase Connect', 'Prism Project'] as const
 export const streamersMap: Map<YouTubeChannelId, Streamer> = new Map(
   streamers.map((s) => [s.ytId, s]),
 )
+
+// TODO make this pretty
+function getStreamerArrayByGroup(group: string) {
+  if (group === 'Hololive') return hololive
+  if (group === 'Nijisanji') return [nijisanji, nijisanji_id, nijisanji_jp, nijisanji_kr].flat()
+  if (group === 'Nijisanji EN') return nijisanji
+  if (group === 'Nijisanji ID') return nijisanji_id
+  if (group === 'Nijisanji KR') return nijisanji_kr
+  if (group === 'Nijisanji JP') return nijisanji_jp
+  if (group === 'Independent') return indies
+  if (group === 'Idol') return IdolComp
+  if (group === 'VShojo') return vshojo
+  if (group === 'Phase Connect') return phase_connect
+  if (group === 'Prism Project') return prism_project
+}
 
 export const streamersYtIdSet: Set<YouTubeChannelId> = new Set(streamers.map((s) => s.ytId))
 
@@ -28,8 +43,13 @@ export const twitters = streamers.map((x) => x.twitter)
 export type StreamerName = typeof names[number] | 'all'
 export type StreamerTwitter = typeof twitters[number]
 
-export function getStreamerList(): string {
-  return streamers.map((streamer) => streamer.name).join(', ')
+function isIN<T>(values: readonly T[], x: any): x is T {
+  return values.includes(x)
+}
+
+export function getStreamerList(group: string): string {
+  const streamerThing = getStreamerArrayByGroup(group)
+  return streamerThing!.map((x) => x.name).join(', ')
 }
 
 export function findStreamerName(name: string): StreamerName | undefined {
@@ -52,8 +72,8 @@ export function replyStreamerList(x: CommandInteraction | ValidatedOptions): voi
   reply(
     msg,
     createEmbed({
-      title: 'Supported channels',
-      description: getStreamerList(),
+      title: 'Supported Streamer Groups',
+      description: streamerGroups.join('\n'),
     }),
   )
 }
