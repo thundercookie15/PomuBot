9d279b5325b87b4ea48043b367fa0e991ab7909c
lucas <lucas.maijers@student.rocvf.nl>
2023-09-04 14:22:31 +0200

    add linebreak

--- src/modules/livechat/chatRelayerWorker.ts
+++ src/modules/livechat/chatRelayerWorker.ts
@@ -264,7 +264,7 @@ function extracted(cmt: ChatComment, isATl: boolean, deepLTl: string | undefined
   let url = '';
 
   if (!cmt.isOwner && g.relay.length > 1 && !isOnlyStreamerRelayedMultipleTimes(g, streamersMap.get(cmt.id)!.name)) {
-    url = `**Chat:** [${frame.channel.name}](https://youtu.be/${frame.id})`;
+    url = `\n**Chat:** [${frame.channel.name}](https://youtu.be/${frame.id})`;
   }
 
   const author = isATl ? `||${cmt.name}:||` : `**${cmt.name}:**`
